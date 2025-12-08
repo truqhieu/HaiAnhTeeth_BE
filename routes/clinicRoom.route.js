8737e5cc2493791656a04619098e5033942d46c8
Thao <thaotvhe170862@fpt.edu.vn>
2025-12-08 15:09:41 +0700

    unlock delete route

--- routes/clinicRoom.route.js
+++ routes/clinicRoom.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken, verifyRole } = require('../middleware/auth.middleware');
-const { createClinicRoom, getAllClinicRooms, viewDetailClinicRoom, updateClinicRoom, listDoctor, assignDoctor, unssignDoctor } = require('../controllers/clinicRoom.controller');
+const { createClinicRoom, getAllClinicRooms, viewDetailClinicRoom, updateClinicRoom, listDoctor, assignDoctor, unssignDoctor, deleteClinicRoom } = require('../controllers/clinicRoom.controller');
 
 router.post('/clinic-rooms', verifyToken, verifyRole('Manager'), createClinicRoom)
 router.get('/clinic-rooms', getAllClinicRooms)
@@ -10,7 +10,7 @@ router.get('/clinic-rooms/:id', viewDetailClinicRoom)
 router.patch('/clinic-rooms/:id', verifyToken, verifyRole('Manager'), updateClinicRoom)
 router.patch('/clinic-rooms/assign-doctor/:id', verifyToken, verifyRole('Manager'), assignDoctor)
 router.patch('/clinic-rooms/unssign-doctor/:id', verifyToken, verifyRole('Manager'), unssignDoctor)
-// router.delete('/clinic-rooms/:id', verifyToken, verifyRole('Manager'), deleteClinicRoom)
+router.delete('/clinic-rooms/:id', verifyToken, verifyRole('Manager'), deleteClinicRoom)
 
 
 module.exports = router;
