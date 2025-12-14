4cd7d7945cce2e6d4663e989d8a1ca833cc4fed8
truqhieu <trunghieu2003hhh@gmail.com>
2025-12-15 01:10:32 +0700

    Fix bug

--- services/leaveRequest.service.js
+++ services/leaveRequest.service.js
@@ -144,7 +144,7 @@ class LeaveRequestService {
     } = filters;
 
     const pageNum = Math.max(1, parseInt(page, 10) || 1);
-    const limitNum = Math.min(100, parseInt(limit, 10) || 10);
+    const limitNum = Math.min(1000, parseInt(limit, 10) || 10);
     const skip = (pageNum - 1) * limitNum;
 
     const filter = {};
